0e9d18616b392ebdd702fdaa95cb89be78a8661a
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2026-03-09 16:18:43 +0200

    initial commit

---
+++ src/main/java/CalculatorController.java
@@ -0,0 +1,32 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+
+    @FXML private TextField number1Field;
+    @FXML private TextField number2Field;
+    @FXML private Label resultLabel;
+
+    @FXML
+    private void onCalculateClick() {
+        try {
+            double num1 = Double.parseDouble(number1Field.getText());
+            double num2 = Double.parseDouble(number2Field.getText());
+
+            double sum = num1 + num2;
+            double product = num1 * num2;
+            double subtraction = num1 - num2;
+            double division = num1 / num2;
+
+            resultLabel.setText("Sum: " + sum + ",Subtraction: " + subtraction + ", Product: " + product + ", Division: " + division);
+
+            // Save to DB
+            ResultService.saveResult(num1, num2, sum, subtraction, product, division);
+
+        } catch (NumberFormatException e) {
+            resultLabel.setText("Please enter valid numbers!");
+        }
+    }
+}
